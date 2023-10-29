6f540a80adb76eb5a35b1dbcf953caa2afab8e0e
fananyamak <s11924009@stu.najah.edu>
2023-10-29 16:35:42 +0200

    neworder

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 let counter=0;
 
 
+
+
+
 class task{
     
      task_done=false;
@@ -25,10 +28,14 @@ class task{
 const node = document.getElementsByClassName("input-text")[0];
 node.addEventListener("keyup", function(event) {
     if (event.key === "Enter") {
-        localStorage.setItem(node.value,"false");
-       
-        buildliitem(node.value)
+       // localStorage.setItem(node.value,"false");
+       const itemOrder = JSON.parse(localStorage.getItem("itemOrder")) || [];
+       itemOrder.push(node.value);
+       console.log(itemOrder);
+        buildliitem(node.value);
+        updateLocalStorageOrder();
                 node.value="";
+                
         // Do work
         
     }
@@ -56,7 +63,7 @@ document.getElementById("item-list").addEventListener("click", function (event) 
             const labelElement = target.closest("label");
             const labelText = labelForCheckbox.textContent;
             
-            localStorage.setItem(labelText, checkbox.checked.toString());
+            updateLocalStorageOrder() ;
         }
 
         // Check if the "Delete" button was clicked
@@ -70,10 +77,23 @@ document.getElementById("item-list").addEventListener("click", function (event) 
             // const items = JSON.parse(localStorage.getItem("items")) || [];
             // const updatedItems = items.filter((item) => item !== listItemValue);
             // localStorage.setItem("items", JSON.stringify(updatedItems));
-            localStorage.removeItem(labelValue);
+            //localStorage.removeItem(labelValue);
 
             // Remove the list item from the DOM
             listItem.remove();
+            const itemOrder = JSON.parse(localStorage.getItem("itemOrder")) || [];
+const indexToRemove = itemOrder.indexOf(labelValue);
+
+if (indexToRemove !== -1) {
+    // Remove the item from the item order array
+    itemOrder.splice(indexToRemove, 1);
+
+    // Update the item order in local storage
+    localStorage.setItem("itemOrder", JSON.stringify(itemOrder));
+
+    // Remove the item from the list
+    // You might need to add code to remove the HTML element from the DOM
+}
         }
         const menu = target.closest('.item').querySelector('.menu');
         menu.style.display = "none";
@@ -113,6 +133,149 @@ document.querySelectorAll(".tab label").forEach((tabLabel) => {
         filterItems(filter);
     });
 });
+// const list = document.getElementById("item-list");
+
+// let draggedItem = null;
+
+// list.addEventListener("dragstart", (e) => {
+//     if (e.target.tagName === "I" && e.target.classList.contains("fa-grip-vertical")) {
+//         const listItem = e.target.closest("li");
+//         if (listItem) {
+//             draggedItem = listItem;
+//             draggedItem.classList.add("dragging");
+        
+//             e.dataTransfer.setData("text/plain", draggedItem.dataset.index);
+//         }
+//     }else {
+        
+//         e.preventDefault(); // Prevent dragging other elements
+//     }
+// });
+// const icons = document.querySelectorAll(".fa-grip-vertical");
+
+// icons.forEach((icon) => {
+//     icon.addEventListener("dragstart", (e) => {
+//         const listItem = icon.closest("li");
+//         listItem.style.display = "none"; // Hide the list item
+//         e.dataTransfer.setDragImage(listItem, 0, 0); // Set the drag image to the entire list item
+//     });
+// });
+// list.addEventListener("dragend", (e) => {
+//     const draggedItem = document.querySelector(".dragging");
+//         draggedItem.classList.remove("dragging");
+// });
+
+// list.addEventListener("dragover", (e) => {
+//     e.preventDefault();
+//     if (draggedItem) {
+//         const dropTarget = e.target.closest("li");
+//         if (dropTarget) {
+//             // Remove the "dragged" class from any previously dragged item
+//             const previouslyDragged = list.querySelector(".dragged");
+//             if (previouslyDragged) {
+//                 previouslyDragged.classList.remove("dragged");
+//             }
+//             dropTarget.classList.add("dragged");
+//         }
+//     }
+// });
+// list.addEventListener("dragleave", (e) => {
+//     if (draggedItem) {
+//         const dropTarget = e.target.closest("li");
+//         if (dropTarget) {
+//             dropTarget.classList.remove("dragged");
+//         }
+//     }
+// });
+
+// list.addEventListener("drop", (e) => {
+    
+//     e.preventDefault();
+//     let dropTarget = e.target;
+
+    
+//         dropTarget = dropTarget.closest("li");
+    
+   
+
+
+//     // Swap the positions of the dragged item and the drop target
+//     if (draggedItem && dropTarget && draggedItem !== dropTarget) {
+//         const items = list.querySelectorAll("li");
+//         const draggedIndex = Array.from(items).indexOf(draggedItem);
+//         const dropIndex = Array.from(items).indexOf(dropTarget);
+
+//         if (draggedIndex !== -1 && dropIndex !== -1) {
+//             if (draggedIndex < dropIndex) {
+//                 list.insertBefore(draggedItem, dropTarget.nextSibling);
+//             } else {
+//                 list.insertBefore(draggedItem, dropTarget);
+//             }
+//         }
+
+//         const Stitems = document.querySelectorAll('.fa-ul > li');
+//         const updatedOrder = Array.from(Stitems).map((item) => item.querySelector('.item-label').textContent);
+       
+//         // Step 3: Clear the old data from local storage
+       
+    
+//         // Step 4: Store the updated data with the new order back into local storage
+//         updatedOrder.forEach((value, index) => {
+           
+//           localStorage.setItem(value, Stitems[index].querySelector('.item-check').checked.toString());
+//           console.log(value);
+//         });
+//         for (let i = 0; i < localStorage.length; i++) {
+//             const key = localStorage.key(i);
+//             const value = localStorage.getItem(key);
+//             console.log(`Key: ${key}, Value: ${value}`);
+//         }
+//     }
+//     dropTarget.classList.remove("dragged");
+//     draggedItem.classList.remove("dragged");
+
+//     draggedItem = null;
+// });
+
+// const icons = document.querySelectorAll(".fa-grip-vertical");
+
+// icons.forEach((icon) => {
+//     icon.addEventListener("dragstart", (e) => {
+//         const listItem = icon.closest("li");
+//         e.dataTransfer.setDragImage(listItem, 0, 0);
+//         listItem.classList.add("dragged");
+//         const clonedItem = listItem.cloneNode(true);
+//         clonedItem.classList.add("clone");
+//         document.body.appendChild(clonedItem);
+//     });
+// });
+
+// document.body.addEventListener("dragend", () => {
+//     const clonedItem = document.querySelector(".clone");
+//     if (clonedItem) {
+//         clonedItem.parentNode.removeChild(clonedItem);
+//     }
+//     const draggedItem = document.querySelector(".dragged");
+//     if (draggedItem) {
+//         draggedItem.classList.remove("dragged");
+//     }
+// });
+
+// const itemList = document.getElementById("item-list");
+
+// itemList.addEventListener("dragover", (e) => {
+//     e.preventDefault();
+//     const draggedItem = document.querySelector(".dragged");
+//     if (draggedItem) {
+//         listItem.style.display = "none"; // Hide the list item
+//         const li = document.elementFromPoint(e.clientX, e.clientY);
+//         if (li && li.tagName === "LI" && !li.classList.contains("dragged")) {
+//             itemList.insertBefore(draggedItem, li);
+//         }
+
+//     }
+// });
+
 const list = document.getElementById("item-list");
 
 let draggedItem = null;
@@ -173,26 +336,10 @@ list.addEventListener("drop", (e) => {
                 list.insertBefore(draggedItem, dropTarget);
             }
         }
-
-        const Stitems = document.querySelectorAll('.fa-ul > li');
-        const updatedOrder = Array.from(Stitems).map((item) => item.querySelector('.item-label').textContent);
-       
-        // Step 3: Clear the old data from local storage
-       
-    
-        // Step 4: Store the updated data with the new order back into local storage
-        updatedOrder.forEach((value, index) => {
-           
-          localStorage.setItem(value, Stitems[index].querySelector('.item-check').checked.toString());
-          console.log(value);
-        });
-        for (let i = 0; i < localStorage.length; i++) {
-            const key = localStorage.key(i);
-            const value = localStorage.getItem(key);
-            console.log(`Key: ${key}, Value: ${value}`);
-        }
     }
+    localStorage.clear();
     draggedItem.classList.remove("dragged");
+    updateLocalStorageOrder();
 
     draggedItem = null;
 });
